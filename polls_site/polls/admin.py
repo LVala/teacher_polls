8fa0b6005e3ebbf04b7b69977f5d58e7aff034d0
LVala <84684231+LVala@users.noreply.github.com>
2022-02-25 23:27:08 +0100

    Add administration site improvements

--- polls_site/polls/admin.py
+++ polls_site/polls/admin.py
@@ -2,6 +2,21 @@ from django.contrib import admin
 
 from .models import Teacher, Subject, Choice
 
+class ChoiceAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['teacher', 'subject']}),
+        ('Voting - values', {'fields': ['total_voters_number', 'total_quality', 'total_friendliness', 'total_motivation', 'total_ease']}),
+    ]
+
+    list_display = ('teacher', 'subject')
+
+    list_filter = ['subject']
+
+admin.site.register(Choice, ChoiceAdmin)
 admin.site.register(Teacher)
 admin.site.register(Subject)
-admin.site.register(Choice)
+
+# change admin site headers etc.
+admin.site.site_header = 'Teacher polls administration'
+admin.site.site_title = 'Administration'
+admin.site.index_title = 'Teacher polls'
